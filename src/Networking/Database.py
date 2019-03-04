489a1d33c0deb8e2ec7109c55fa122a179ea1f9f
Daniel <danielip@buffalo.edu>
2019-03-04 13:40:07 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	ArenaProject.iml

--- src/Networking/Database.py
+++ src/Networking/Database.py
@@ -1,11 +1,1 @@
-import sqlite3
 
-conn = sqlite3.connect('game.db')
-cur = conn.cursor()
-
-cur.execute('CREATE TABLE IF NOT EXISTS players (name, playerid)')
-
-def insert(name, playerid):
-    cur.execute('INSERT INTO players VALUES (?,?)', (name, playerid))
-
-insert("Can", "9")
