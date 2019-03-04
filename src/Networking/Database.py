6c0df5942e7ec277d21ed757fb60dcaed32980e9
jonchoi <jmchoi@buffalo.edu>
2019-03-04 15:41:51 -0500

    jo

--- src/Networking/Database.py
+++ src/Networking/Database.py
@@ -1,1 +1,11 @@
+import sqlite3
 
+conn = sqlite3.connect('atest.db')
+
+cur = conn.cursor()
+
+cur.execute(
+    'CREATE TABLE IF NOT EXISTS movies (title, director, year)')
+
+cur.execute(
+    'INSERT INTO movies VALUES ("Jaws", "Spielberg", 1975)')
