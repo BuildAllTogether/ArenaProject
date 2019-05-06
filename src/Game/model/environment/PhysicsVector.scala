a5d23f7165befb9f3771dd7ee070efc11d6be4ad
Daniel <danielip@buffalo.edu>
2019-05-06 15:01:09 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	ArenaProject.iml

--- src/Game/model/environment/PhysicsVector.scala
+++
@@ -1,9 +0,0 @@
-package Game.model.environment
-
-class PhysicsVector(var x: Double, var y: Double, var z: Double) {
-
-  override def toString: String = {
-  "(" + x + ", " + y + ", " + z + ")"
-}
-
-}
--- src/Game/model/game_objects/PhysicalObject.scala
+++
@@ -1,7 +0,0 @@
-package Game.model.game_objects
-
-import Game.model.environment.PhysicsVector
-
-class PhysicalObject(val location: PhysicsVector, val velocity: PhysicsVector) {
-
-}
